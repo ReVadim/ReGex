bd635fc6458b359a3f265f80b660faadce9a297a
ReVadim <revani@yandex.ru>
2021-04-04 22:48:19 +0300

    recovered version, simple line-code

--- test.py
+++ test.py
@@ -3,10 +3,6 @@ import re
 
 
 pattern_dict = {
-    'fio': {
-        'in': r'([А-ЯЁ]\w+)\s*,*([А-ЯЁ]\w+)\s*,*([А-ЯЁ]\w+\s*)*',
-        'out': r'\1 \2 \3'
-    },
     'phone_num': {
         'in': r'(\+7\s*|8\s*)\(*(\d{3})\)*\s*-*(\d{3})-*\s*(\d{2})-*\s*(\d{2})',
         'out': r'8(\2)\3-\4-\5'
@@ -21,22 +17,48 @@ pattern_dict = {
     }
 }
 
-new_dict = {}
-output_list = []
-# def csv_reader():
 with open("phonebook_raw.csv", encoding='utf-8') as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
 
-for line in contacts_list:
-    line = ','.join(line)
+
+def text_normalizer(text_list):
+    for word in text_list:
+        if not word:
+            continue
+        for split_words in word.split():
+            yield split_words
+
+
+def row_converter(text):
+    text = ','.join(text)
     for key, item in pattern_dict.items():
-        line = re.sub(item['in'], item['out'], line)
-    output_list.append(line)
+        text = re.sub(item['in'], item['out'], text)
+    return text.split(',')
 
-# for row in output_list:
-#     print(row)
 
-with open("phonebook.csv", "w", encoding='utf-8') as phonebook:
-    datawriter = csv.writer(phonebook, delimiter=',')
-    datawriter.writerows(output_list)
+def list_converter(text_list):
+    converted_rows = []
+    for row in text_list:
+        if len((row[0]).split()) == 2:
+            row.insert(3, '')
+        row = row_converter(row)
+        row[:3] = text_normalizer(row[:3])
+        converted_rows.append(row)
+    return converted_rows
+
+
+def join_data(data_list):
+    fixed_info_list = []
+    for i in range(len(data_list)):
+        for j in range(len(data_list)):
+            if data_list[i][0] == data_list[j][0]:
+                data_list[i] = [x or y for x, y in zip(data_list[i], data_list[j])]
+        if data_list[i] not in fixed_info_list:
+            fixed_info_list.append(data_list[i])
+    return fixed_info_list
+
+
+result = join_data(list_converter(contacts_list))
+for line in result:
+    print(line)
